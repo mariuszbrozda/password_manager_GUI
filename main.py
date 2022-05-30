f07e1bcf5f721a7d779e60652dd99fa7f459af00
Mariusz <mariuszbrozda.dublin@gmail.com>
2022-05-30 20:55:24 +0100

    Refactor code to write, read, update data in json file

--- main.py
+++ main.py
@@ -45,8 +45,15 @@ def save():
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        with open("data.json", "w+") as data_file:
-            json.dump(new_data, data_file, indent=4)
+        with open("data.json", "r") as data_file:
+            #Reading old data
+            data = json.load(data_file)
+            # Update old data with new data
+            data.update(new_data)
+
+        with open("data.json", "w") as data_file:
+            # Saving updated data
+            json.dump(data, data_file, indent=4)
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
